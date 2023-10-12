cf6466ab53657b494d24c09dfe92ff87e55d6404
Thai Quang <thaiquangqt2003@gmail.com>
2023-10-13 00:56:52 +0700

    coding

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./contact.scss";
 import CustomInput from "../../components/customInput/CustomInput";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
@@ -42,11 +43,52 @@ const Contact = () => {
                     <label htmlFor="message">Your message *</label>
                     <textarea id="message" rows={10} cols={5} />
                   </div>
+                  <button className="btn" type="submit">
+                    Send Message
+                  </button>
                 </form>
               </div>
             </div>
           </div>
-          <div className="col-4"></div>
+          <div className="col-4">
+            <div className="contact-store">
+              <div className="store-header">
+                <ion-icon name="home-outline"></ion-icon>
+                <h4>Clotya Store</h4>
+              </div>
+              <div className="store-content">
+                <p>
+                  Germany — 785 15h Street, Office 478/B <br /> Green Mall
+                  Berlin, De 81566
+                </p>
+                <p>
+                  Phone: +1 1234 567 88 <br />
+                  Email: <Link>contact@example.com</Link>
+                </p>
+              </div>
+            </div>
+            <div className="contact-store">
+              <div className="store-header">
+                <ion-icon name="home-outline"></ion-icon>
+                <h4>Opening Hours</h4>
+              </div>
+              <div className="store-content">
+                <p>
+                  Monday - Friday : 9am - 5pm <br />
+                  Weekend Closed
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="contact-des">
+          <p>
+            In hac habitasse platea dictumst. Pellentesque viverra sem nec orci
+            lacinia, in bibendum urna mollis. Quisque nunc lacus, varius vel leo
+            a, pretium lobortis metus. Vivamus consectetur consequat justo. Sed
+            interdum nunc ut tristique congue. Quisque maximus mauris et dui
+            sagittis scelerisque.
+          </p>
         </div>
       </div>
     </div>
